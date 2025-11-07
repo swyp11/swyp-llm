6afb506991259a3f549e2ba76e8a48ee42761972
Ahnyeongjun <toinin@naver.com>
2025-11-07 19:27:15 +0900

    fix: 없는 레포 (불필요) 제거

--- src/database/repositories/__init__.py
+++ src/database/repositories/__init__.py
@@ -1,10 +1,6 @@
 # Database repositories
 from src.database.repositories.recommendation import recommendation_repo
-from src.database.repositories.wedding_dress import wedding_dress_repo
-from src.database.repositories.survey import survey_repo
 
 __all__ = [
-    "recommendation_repo",
-    "wedding_dress_repo",
-    "survey_repo"
+    "recommendation_repo"
 ]
